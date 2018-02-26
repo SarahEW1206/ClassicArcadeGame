9724cb6ae9d2a3c3b328cb8dede6e9dd634f4bf6
Ul1ra <benpcunningham@outlook.com>
2018-02-26 09:10:39 +0100

    fixed setTimout using =>

--- js/app.js
+++ js/app.js
@@ -87,11 +87,10 @@ Player.prototype.handleInput = function (keyPress) {
     // Once the user reaches the top of the page; the water, the user is
     // Instantly reset to the starting position
     if (this.y < 0) {
-        setTimeout(function () {
+        setTimeout(() => {
             this.x = 202;
             this.y = 405;
-        }, 600);
-
+        }, 800);
     };
 };
 
